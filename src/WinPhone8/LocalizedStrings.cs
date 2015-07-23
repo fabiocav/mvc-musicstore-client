e1c47d6549b812cac30340eebd9aef49eb597712
Fabio Cavalcante <facaval@microsoft.com>
2015-07-23 14:47:18 -0700

    Initial commit

---
+++ src/WinPhone8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using zumomusicstore.WinPhone.Resources;
+
+namespace zumomusicstore.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ src/zumomusicstore/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zumomusicstore
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services url and key.
+		public static string ApplicationURL = @"https://zumomusicstore.azurewebsites.net";
+		public static string ApplicationKey = @"";
+	}
+}
+
